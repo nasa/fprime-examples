fcea6324d7a5160422a212c3a8077c6d0e3047ab
thomas-bc <thomas.boyerchammard@gmail.com>
2025-08-20 12:01:26 -0700

    Update DecafDeframer to extract APID from data

--- FlightExamples/CustomFraming/DecafDeframer/DecafDeframer.cpp
+++ FlightExamples/CustomFraming/DecafDeframer/DecafDeframer.cpp
@@ -26,10 +26,11 @@ DecafDeframer ::~DecafDeframer() {}
 // ----------------------------------------------------------------------
 
 void DecafDeframer ::dataIn_handler(FwIndexType portNum, Fw::Buffer& data, const ComCfg::FrameContext& context) {
-    if (data.getSize() < CustomFraming::Types::FrameHeader::SERIALIZED_SIZE + CustomFraming::Types::FrameTrailer::SERIALIZED_SIZE) {
+    if (data.getSize() <
+        CustomFraming::Types::FrameHeader::SERIALIZED_SIZE + CustomFraming::Types::FrameTrailer::SERIALIZED_SIZE) {
         // Incoming buffer is not long enough to contain a valid frame (header+trailer)
         this->log_WARNING_HI_InvalidBufferReceived();
-        this->dataReturnOut_out(0, data, context); // drop the frame
+        this->dataReturnOut_out(0, data, context);  // drop the frame
         return;
     }
 
@@ -46,7 +47,7 @@ void DecafDeframer ::dataIn_handler(FwIndexType portNum, Fw::Buffer& data, const
     const CustomFraming::Types::FrameHeader defaultValue;
     if (header.get_startWord() != defaultValue.get_startWord()) {
         this->log_WARNING_HI_InvalidStartWord();
-        this->dataReturnOut_out(0, data, context); // drop the frame
+        this->dataReturnOut_out(0, data, context);  // drop the frame
         return;
     }
     // We expect the frame size to be size of header + body (of size specified in header) + trailer
@@ -54,13 +55,26 @@ void DecafDeframer ::dataIn_handler(FwIndexType portNum, Fw::Buffer& data, const
                                          CustomFraming::Types::FrameTrailer::SERIALIZED_SIZE;
     if (data.getSize() < expectedFrameSize) {
         this->log_WARNING_HI_InvalidLengthReceived();
-        this->dataReturnOut_out(0, data, context); // drop the frame
+        this->dataReturnOut_out(0, data, context);  // drop the frame
         return;
+    }
+
+    // -------- Attempt to extract APID from Payload --------
+    // If PacketDescriptor translates to an invalid APID, let it default to FW_PACKET_UNKNOWN
+    // and let downstream components (e.g. custom router) handle it
+    FwPacketDescriptorType packetDescriptor;
+    status = deserializer.deserialize(packetDescriptor);
+    FW_ASSERT(status == Fw::SerializeStatus::FW_SERIALIZE_OK, status);
+    ComCfg::FrameContext contextCopy = context;
+    // If a valid descriptor is deserialized, set it in the context
+    if (packetDescriptor < ComCfg::APID::INVALID_UNINITIALIZED) {
+        contextCopy.set_apid(static_cast<ComCfg::APID::T>(packetDescriptor));
     }
 
     // ---------------- Validate Frame Trailer ----------------
     // Deserialize transmitted trailer: trailer is at offset = len(header) + len(body)
-    status = deserializer.moveDeserToOffset(CustomFraming::Types::FrameHeader::SERIALIZED_SIZE + header.get_lengthField());
+    status =
+        deserializer.moveDeserToOffset(CustomFraming::Types::FrameHeader::SERIALIZED_SIZE + header.get_lengthField());
     FW_ASSERT(status == Fw::SerializeStatus::FW_SERIALIZE_OK, status);
     status = trailer.deserialize(deserializer);
     FW_ASSERT(status == Fw::SerializeStatus::FW_SERIALIZE_OK, status);
@@ -77,7 +91,7 @@ void DecafDeframer ::dataIn_handler(FwIndexType portNum, Fw::Buffer& data, const
     // Check that the CRC in the trailer of the frame matches the computed CRC
     if (trailer.get_crcField() != computedCrc.asBigEndianU32()) {
         this->log_WARNING_HI_InvalidChecksum();
-        this->dataReturnOut_out(0, data, context); // drop the frame
+        this->dataReturnOut_out(0, data, context);  // drop the frame
         return;
     }
 
@@ -87,13 +101,14 @@ void DecafDeframer ::dataIn_handler(FwIndexType portNum, Fw::Buffer& data, const
     // Shrink size to effectively remove the trailer (also removes the header)
     data.setSize(data.getSize() - CustomFraming::Types::FrameHeader::SERIALIZED_SIZE -
                  CustomFraming::Types::FrameTrailer::SERIALIZED_SIZE);
-    // Emit the deframed data
-    this->dataOut_out(0, data, context);
+    // Emit the deframed data with updated APID in context
+    this->dataOut_out(0, data, contextCopy);
 }
 
-void DecafDeframer ::dataReturnIn_handler(FwIndexType portNum, Fw::Buffer& fwBuffer, const ComCfg::FrameContext& context) {
+void DecafDeframer ::dataReturnIn_handler(FwIndexType portNum,
+                                          Fw::Buffer& fwBuffer,
+                                          const ComCfg::FrameContext& context) {
     this->dataReturnOut_out(0, fwBuffer, context);
 }
-
 
 }  // namespace CustomFraming
